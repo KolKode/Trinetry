8351d2463833effc6008f9af89be2fa9f724b85e
mrsupradip <supradipmajumdar55@gmail.com>
2025-06-03 00:39:31 +0530

    Back button added

--- app/src/main/java/io/github/kolkode/trinetry/import_eth.java
+++ app/src/main/java/io/github/kolkode/trinetry/import_eth.java
@@ -1,7 +1,9 @@
 package io.github.kolkode.trinetry;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class import_eth extends AppCompatActivity {
     Button recoverybtn;
     Button privatebtn;
+    Button backbtn;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -27,6 +30,15 @@ public class import_eth extends AppCompatActivity {
         });
         recoverybtn=findViewById(R.id.recoverybtn);
         privatebtn=findViewById(R.id.privatebtn);
+        backbtn=findViewById(R.id.backbtn);
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(import_eth.this,welcome_page.class);
+                startActivity(intent);
+            }
+        });
 
 
     }
