be7cbe9d427f547bc3d37ba6b28b68b1fbd8b50a
mrsupradip <supradipmajumdar55@gmail.com>
2025-06-03 15:36:36 +0530

    Checkbox logic

--- app/src/main/java/io/github/kolkode/trinetry/welcome_page.java
+++ app/src/main/java/io/github/kolkode/trinetry/welcome_page.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +14,7 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class welcome_page extends AppCompatActivity {
-    Button agraybtn;
+    CheckBox agraybtn;
     Button newaccbtn;
     Button alreadybtn;
 
@@ -23,27 +24,36 @@ public class welcome_page extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_welcome_page);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        agraybtn=findViewById(R.id.agraybtn);
-        newaccbtn=findViewById(R.id.newaccbtn);
-        alreadybtn=findViewById(R.id.alreadyaccbtn);
 
+        agraybtn = findViewById(R.id.agraybtn); // Make sure this is CheckBox
+        newaccbtn = findViewById(R.id.newaccbtn);
+        alreadybtn = findViewById(R.id.alreadyaccbtn);
+
+        // Disable other buttons
+        newaccbtn.setEnabled(false);
+        alreadybtn.setEnabled(false);
+
+        // Set up checkbox listener
+        agraybtn.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            newaccbtn.setEnabled(isChecked);
+            alreadybtn.setEnabled(isChecked);
+        });
+
+        // Button click listeners
         alreadybtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent=new Intent(welcome_page.this, import_eth.class);
+                Intent intent = new Intent(welcome_page.this, import_eth.class);
                 startActivity(intent);
-
             }
         });
 
 
-
-
     }
 }
-
