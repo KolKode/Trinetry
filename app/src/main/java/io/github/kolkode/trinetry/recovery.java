02b9f1e97e48ab5b8b340077880826df88e61b53
mrsupradip <supradipmajumdar55@gmail.com>
2025-06-03 13:52:26 +0530

    Recovery page

--- app/src/main/java/io/github/kolkode/trinetry/recovery.java
+++ app/src/main/java/io/github/kolkode/trinetry/recovery.java
@@ -1,10 +1,10 @@
 package io.github.kolkode.trinetry;
 
-import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,41 +12,47 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class import_eth extends AppCompatActivity {
-    Button recoverybtn;
-    Button privatebtn;
-    Button backbtn;
+public class recovery extends AppCompatActivity {
 
-    @SuppressLint("MissingInflatedId")
+    private Button subbtn;
+    private Button backbtn;
+    private EditText p1txt, p2txt, p3txt, p4txt, p5txt, p6txt,
+            p7txt, p8txt, p9txt, p10txt, p11txt, p12txt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_import_eth);
+        setContentView(R.layout.activity_recovery);
+
+        // Find views after layout is set
+        subbtn = findViewById(R.id.subbtn);
+        backbtn = findViewById(R.id.backbtn);
+
+        p1txt = findViewById(R.id.seed1);
+        p2txt = findViewById(R.id.seed2);
+        p3txt = findViewById(R.id.seed3);
+        p4txt = findViewById(R.id.seed4);
+        p5txt = findViewById(R.id.seed5);
+        p6txt = findViewById(R.id.seed6);
+        p7txt = findViewById(R.id.seed7);
+        p8txt = findViewById(R.id.seed8);
+        p9txt = findViewById(R.id.seed9);
+        p10txt = findViewById(R.id.seed10);
+        p11txt = findViewById(R.id.seed11);
+        p12txt = findViewById(R.id.seed12);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        recoverybtn=findViewById(R.id.recoverybtn);
-        privatebtn=findViewById(R.id.privatebtn);
-        backbtn=findViewById(R.id.backbtn);
 
         backbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              finish();
+                finish();
             }
         });
-
-        recoverybtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(import_eth.this, recovery.class);
-                startActivity(intent);
-            }
-        });
-
-
     }
 }
