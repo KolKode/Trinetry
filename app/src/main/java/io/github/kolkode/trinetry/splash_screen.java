786cd4f3b5372e5777974a683812a7de04915ece
mrsupradip <supradipmajumdar55@gmail.com>
2025-06-04 14:16:55 +0530

    splash_screen added

---
+++ app/src/main/java/io/github/kolkode/trinetry/splash_screen.java
@@ -0,0 +1,26 @@
+package io.github.kolkode.trinetry;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class splash_screen extends AppCompatActivity {
+
+    private static final long SPLASH_DELAY = 3000; // 3 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Navigate to the Welcome Page
+                Intent intent = new Intent(splash_screen.this, welcome_page.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_DELAY);
+    }
+}
