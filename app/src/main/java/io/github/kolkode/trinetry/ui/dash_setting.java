1e97a877c51e1ea12c57098ad86ea0143c9edf03
mrsupradip <supradipmajumdar55@gmail.com>
2025-07-20 02:21:10 +0530

    some more changes in ui

--- app/src/main/java/io/github/kolkode/trinetry/ui/dash_setting.java
+++ app/src/main/java/io/github/kolkode/trinetry/ui/dash_setting.java
@@ -1,6 +1,10 @@
 package io.github.kolkode.trinetry.ui;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.Switch;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +15,9 @@ import androidx.core.view.WindowInsetsCompat;
 import io.github.kolkode.trinetry.R;
 
 public class dash_setting extends AppCompatActivity {
+    Button rcph_btn,pvtkey_btn;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
+    Switch test_swtch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +29,17 @@ public class dash_setting extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        pvtkey_btn = findViewById(R.id.pvtkey_btn);
+        rcph_btn = findViewById(R.id.rcph_btn);
+        test_swtch = findViewById(R.id.test_swtch);
+
+        rcph_btn.setOnClickListener(v ->{
+            startActivity(new Intent(dash_setting.this,show_recovery.class));
+        });
+        pvtkey_btn.setOnClickListener(v ->{
+            startActivity(new Intent(dash_setting.this,show_private.class));
+        });
+
     }
 }
